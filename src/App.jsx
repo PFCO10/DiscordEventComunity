09df9761519d76f70e3af5be46b296be0a2eeca6
PFCO10 <hergopablofco@gmail.com>
2025-06-18 09:49:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import ServerStats from "./components/ServerStats";
 
+function App() {
+  return (
+    <div>
+      <ServerStats />
+    </div>
+  );
+}
+
+export default App;
